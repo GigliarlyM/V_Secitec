d96ae2f270c8a9fd8005a8ee87432a0c5ed8f688
Gigliárly Marcelino <60317202+GigliarlyM@users.noreply.github.com>
2022-06-22 17:02:20 -0300

    Inicial-02

--- pages-php/cadastro.php
+++ pages-php/cadastro.php
@@ -1,5 +1,15 @@
 <?php
 
-echo "verdade";
+$tipo = $_POST['btn_confir'];
+
+if ($tipo == 'Participe') {
+
+	$nome = $_POST['nome_participante'];
+	echo "Deu certo $nome";
+} else {
+
+	$nome = $_POST['nome_atividade'];
+	echo "Certo, nome do evento $nome";
+}
 
 ?>
