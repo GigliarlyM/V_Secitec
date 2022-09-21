df804dea782b88a105c5decb6ade9af673ccbb1b
Gigliárly Marcelino <60317202+GigliarlyM@users.noreply.github.com>
2022-09-21 19:03:19 -0300

    V2.3

--- pages-php/conexao.php
+++ pages-php/conexao.php
@@ -4,8 +4,8 @@
 
 $hostname = "localhost";
 $dbname = "secitec";
-$usuario = "teste";
-$senha = "teste";
+$usuario = "ifpb";
+$senha = "ifpb";
 
 try {
 	$bd= new PDO("mysql:host=$hostname; dbname=$dbname; charset=utf8mb4", $usuario, $senha);
--- pages-php/participante.php
+++ pages-php/participante.php
@@ -1,9 +1,9 @@
 <?php
 
-include "conexao.php";
-
 if (isset($_POST["tipo_form"])) {
-	echo "enviou";
+
+	include "conexao.php";
+
 	$nome = $_POST['nome_part'];
 	$email = $_POST['email_part'];
 	$telefone = $_POST['telefone_part'];
@@ -12,22 +12,24 @@ if (isset($_POST["tipo_form"])) {
 	$senha = password_hash($_POST['senha_part'], PASSWORD_DEFAULT);
 
 	try {
-		
 		$bd->beginTransaction();
-		
+	
 		$insertParticipante = "INSERT INTO participante (nome,email,telefone,dataNasc)
-    	VALUES (?,?,?,?)";
+    		VALUES (?,?,?,?)";
+
     	$statement = $bd->prepare($insertParticipante);
 	
     	$statement->execute(array($nome, $email, $telefone, $data));
 	
     	$participante_id = $bd->lastInsertId();
-	
-    	$insertUsuario = "INSERT INTO usuarios (nome,senha)
-    	VALUES (?,?,?)";
+		
+    	$insertUsuario = "INSERT INTO  usuario (usuario, senha)
+    		VALUES (?,?)";
+
+    		echo "alert($usuario, $senha)";
     	$statement = $bd->prepare($insertUsuario);
 	
-    	$statement->execute(array($usuario,$senha));
+    	$statement->execute(array($usuario, $senha));
 	
     	$bd->commit();
 
